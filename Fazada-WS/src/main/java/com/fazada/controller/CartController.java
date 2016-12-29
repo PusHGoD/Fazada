d0d90739caedcf4d15a5c463a27f9023710781ff
PusHGoD <vip_pro@live.com>
2016-12-29 15:23:49 +0700

    Update

--- Fazada-WS/src/main/java/com/fazada/controller/CartController.java
+++ Fazada-WS/src/main/java/com/fazada/controller/CartController.java
@@ -3,10 +3,9 @@ package com.fazada.controller;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fazada.model.Product;
 
 @RestController
 @RequestMapping("cart")
@@ -18,7 +17,7 @@ public class CartController {
 	private static Logger logger = Logger.getLogger(CartController.class.getName());
 
 	@RequestMapping(value = "/addToCart")
-	public ResponseEntity<String> addToCart(Product product, int quantity) {
+	public ResponseEntity<String> addToCart(@RequestBody String json) {
 		logger.info("Not implemented");
 		return new ResponseEntity<String>("1", HttpStatus.OK);
 	}
